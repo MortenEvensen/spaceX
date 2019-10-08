a2d766bd27d0539b598b24ad860b2b2a5ae58b9c
MortenEvensen <evensenmorten@hotmail.com>
2019-10-08 23:48:24 +0200

    Added regular expressions to check input in contact form

--- script.js
+++ script.js
@@ -162,3 +162,37 @@ function openMenu() {
 		x.className = "topnav";
 	}
 }
+
+// Contact validation 
+
+document.getElementById("submit-contact").addEventListener("click", function() {
+    
+    var firstName = document.getElementById("first-name").value;
+     var lastName =
+        document.getElementById("last-name").value;
+    var email = document.getElementById("email").value;
+    
+    var firstNameRegex = /[a-zA-Z]/;
+    var lastNameRegex = /[a-zA-Z]/;
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$/;
+    
+    var firstNameResult = firstNameRegex.test(firstName);
+    var lastNameResult = lastNameRegex.test(lastName);
+    var emailResult = emailRegex.test(email);
+    
+    if (firstNameResult === false) {
+        document.getElementById("firstNameError").style.display = "block"
+    } else {
+        document.getElementById("firstNameError").style.display = "none"
+    };
+    if (lastNameResult === false) {
+        document.getElementById("lastNameError").style.display = "block"
+    } else {
+        document.getElementById("lastNameError").style.display = "none"
+    };
+    if (emailResult === false) {
+        document.getElementById("emailError").style.display = "block"
+    } else {
+        document.getElementById("emailError").style.display = "none"
+    };
+})
