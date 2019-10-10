f7cf4d66457f4d8d7d06a3ca6a06394e1b5b8292
MortenEvensen <evensenmorten@hotmail.com>
2019-10-11 00:14:13 +0200

    Added script for uppcomming, made new view concept on upcomming boxes

--- script.js
+++ script.js
@@ -18,14 +18,20 @@ request.onload = function () {
 	var upcommingDate = document.getElementsByClassName("api3");
 	var upcommingRocketId = document.getElementsByClassName("api4");
 	var upcommingLaunchSite = document.getElementsByClassName("api5");
-
+    
+    var upcommingHeading2 = document.getElementsByClassName("boxApi1");
+	var upcommingDetails2 = document.getElementsByClassName("boxApi2");
+	var upcommingDate2 = document.getElementsByClassName("boxApi3");
+	var upcommingRocketId2 = document.getElementsByClassName("boxApi4");
+	var upcommingLaunchSite2 = document.getElementsByClassName("boxApi5");
+    
 	let output = ""
 	for (var i = 0; i < api1.length; i++) {
 		output += "<div>" + api1[i].message + "</div>";
 	}
 	// Add mission name from api
 	for (var i = 0; i < upcommingHeading.length; i++) {
-		upcommingHeading[i].innerHTML = api1[i].mission_name;
+		upcommingHeading[i].innerHTML = "<h3>Mission name: " + api1[i].mission_name + "</h3>";
 	}
 	// Add details from api
 	for (var i = 0; i < upcommingDetails.length; i++) {
@@ -33,16 +39,40 @@ request.onload = function () {
 	}
 	// Add details from api
 	for (var i = 0; i < upcommingDate.length; i++) {
-		upcommingDate[i].innerHTML = api1[i].launch_date_local;
+		upcommingDate[i].innerHTML = "<p>Date: " + api1[i].launch_date_local + "</p>";
 	}
 	// Add details from api
 	for (var i = 0; i < upcommingRocketId.length; i++) {
-		upcommingRocketId[i].innerHTML = api1[i].rocket.rocket_name;
+		upcommingRocketId[i].innerHTML = "<p>Rocket Name: " + api1[i].rocket.rocket_name + "</p>";
 	}
 	for (var i = 0; i < upcommingLaunchSite.length; i++) {
-		upcommingLaunchSite[i].innerHTML = api1[i].launch_site.site_name_long;
+		upcommingLaunchSite[i].innerHTML = "<p>Launch site: " + api1[i].launch_site.site_name_long + "</p>";
 	}
 
+    // Get info for openBox in upcomming launches
+    for (var i = 0; i < upcommingHeading2.length; i++) {
+		upcommingHeading2[i].innerHTML = "<p>Mission name: " +  api1[i].mission_name + "</p>";
+	}
+	// Add details from api
+	for (var i = 0; i < upcommingDetails2.length; i++) {
+		upcommingDetails2[i].innerHTML = "<p>About the mission: " + api1[i].details + "</p>";
+        if (upcommingDetails2[i] == null) {
+         upcommingDetails2[i].innerHTML = "<p></p>";   
+        }
+	} 
+	// Add details from api
+	for (var i = 0; i < upcommingDate2.length; i++) {
+		upcommingDate2[i].innerHTML = "<p>Launch date: " + api1[i].launch_date_local + "<p/>";
+        
+	}
+	// Add details from api
+	for (var i = 0; i < upcommingRocketId2.length; i++) {
+		upcommingRocketId2[i].innerHTML = "<p>Rocket name: " + api1[i].rocket.rocket_name + "</p>";
+	}
+	for (var i = 0; i < upcommingLaunchSite2.length; i++) {
+		upcommingLaunchSite2[i].innerHTML = "<p>Launch site : " + api1[i].launch_site.site_name_long + "</p>";
+	}
+    
 	// Use upcomming date to count down next launch on index page
 
 	var countDown = new Date(api1[1].launch_date_local).getTime();
@@ -56,7 +86,7 @@ request.onload = function () {
 		var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 		var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-		document.getElementById("countdown").innerHTML = "<p>Next launch into space: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s </p>";
+		document.getElementById("countdown").innerHTML = "<a href='https://spaceflightnow.com/launch-schedule/'<p>Next launch into space: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s </p></a>";
 		if (timeDifference <= 0) {
 			clearInterval(x);
 			document.getElementById("countdown").innerHTML = "EXPIRED";
---
+++ uppcomming-script.js
@@ -0,0 +1,168 @@
+//Open and closes every box in uppcomming
+
+function openBox() {
+    document.getElementById("box1").style.width = "100%";
+};
+
+function closeBox() {
+    document.getElementById("box1").style.width = "0%";
+};
+
+function openBox2() {
+    document.getElementById("box2").style.width = "100%";
+};
+
+function closeBox2() {
+    document.getElementById("box2").style.width = "0%";
+};
+
+function openBox3() {
+    document.getElementById("box3").style.width = "100%";
+};
+
+function closeBox3() {
+    document.getElementById("box3").style.width = "0%";
+};
+
+function openBox4() {
+    document.getElementById("box4").style.width = "100%";
+};
+
+function closeBox4() {
+    document.getElementById("box4").style.width = "0%";
+};
+
+function openBox5() {
+    document.getElementById("box5").style.width = "100%";
+};
+
+function closeBox5() {
+    document.getElementById("box5").style.width = "0%";
+};
+
+function openBox6() {
+    document.getElementById("box6").style.width = "100%";
+};
+
+function closeBox6() {
+    document.getElementById("box6").style.width = "0%";
+};
+
+function openBox7() {
+    document.getElementById("box7").style.width = "100%";
+};
+
+function closeBox7() {
+    document.getElementById("box7").style.width = "0%";
+};
+
+function openBox8() {
+    document.getElementById("box8").style.width = "100%";
+};
+
+function closeBox8() {
+    document.getElementById("box8").style.width = "0%";
+};
+
+function openBox9() {
+    document.getElementById("box9").style.width = "100%";
+};
+
+function closeBox9() {
+    document.getElementById("box9").style.width = "0%";
+};
+
+function openBox10() {
+    document.getElementById("box10").style.width = "100%";
+};
+
+function closeBox10() {
+    document.getElementById("box10").style.width = "0%";
+};
+
+function openBox11() {
+    document.getElementById("box11").style.width = "100%";
+};
+
+function closeBox11() {
+    document.getElementById("box11").style.width = "0%";
+};
+
+function openBox12() {
+    document.getElementById("box12").style.width = "100%";
+};
+
+function closeBox12() {
+    document.getElementById("box12").style.width = "0%";
+};
+
+function openBox13() {
+    document.getElementById("box13").style.width = "100%";
+};
+
+function closeBox13() {
+    document.getElementById("box13").style.width = "0%";
+};
+
+function openBox14() {
+    document.getElementById("box14").style.width = "100%";
+};
+
+function closeBox14() {
+    document.getElementById("box14").style.width = "0%";
+};
+
+function openBox15() {
+    document.getElementById("box15").style.width = "100%";
+};
+
+function closeBox15() {
+    document.getElementById("box15").style.width = "0%";
+};
+
+function openBox16() {
+    document.getElementById("box16").style.width = "100%";
+};
+
+function closeBox16() {
+    document.getElementById("box16").style.width = "0%";
+};
+
+function openBox17() {
+    document.getElementById("box17").style.width = "100%";
+};
+
+function closeBox17() {
+    document.getElementById("box17").style.width = "0%";
+};
+
+function openBox18() {
+    document.getElementById("box18").style.width = "100%";
+};
+
+function closeBox18() {
+    document.getElementById("box18").style.width = "0%";
+};
+
+function openBox19() {
+    document.getElementById("box19").style.width = "100%";
+};
+
+function closeBox19() {
+    document.getElementById("box19").style.width = "0%";
+};
+
+function openBox20() {
+    document.getElementById("box20").style.width = "100%";
+};
+
+function closeBox20() {
+    document.getElementById("box20").style.width = "0%";
+};
+function openBox21() {
+    document.getElementById("box21").style.width = "100%";
+};
+
+function closeBox21() {
+    document.getElementById("box21").style.width = "0%";
+};
