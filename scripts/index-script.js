205f0571ab4877c088b88d07e16c72fe41d9055e
MortenEvensen <evensenmorten@hotmail.com>
2019-10-13 01:05:57 +0200

    final touches, almost finished

---
+++ scripts/index-script.js
@@ -0,0 +1,17 @@
+let request2 = new XMLHttpRequest();
+request2.open("GET", "https://api.spacexdata.com/v3/info", true);
+
+request2.onload = function() {
+    let api2 = JSON.parse(this.response);
+    document.getElementsByClassName("index-info-api")[0].innerHTML = api2.summary;
+    console.log(api2.summary);
+}
+request2.send();
+
+function openBoxIndex() {
+    document.getElementById("boxIndex").style.width = "100%";
+};
+
+function closeBoxIndex() {
+    document.getElementById("boxIndex").style.width = "0%";
+};
---
+++ scripts/script.js
@@ -0,0 +1,138 @@
+// Fetch api with upcomming launches
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.spacexdata.com/v3/launches/upcoming", true);
+
+request.onload = function() {
+    let api1 = JSON.parse(this.response);
+
+    // Find html elements to target
+    var upcommingHeading = document.getElementsByClassName("api1");
+    var upcommingDetails = document.getElementsByClassName("api2");
+    var upcommingDate = document.getElementsByClassName("api3");
+    var upcommingRocketId = document.getElementsByClassName("api4");
+    var upcommingLaunchSite = document.getElementsByClassName("api5");
+
+    var upcommingHeading2 = document.getElementsByClassName("boxApi1");
+    var upcommingDetails2 = document.getElementsByClassName("boxApi2");
+    var upcommingDate2 = document.getElementsByClassName("boxApi3");
+    var upcommingRocketId2 = document.getElementsByClassName("boxApi4");
+    var upcommingLaunchSite2 = document.getElementsByClassName("boxApi5");
+
+    var upcommingDate3 = document.getElementsByClassName("phoneApi3");
+
+    var upcommingHeadingIndex = document.getElementsByClassName("indexApi1");
+    var upcommingDetailsIndex = document.getElementsByClassName("indexApi2");
+    var upcommingDateIndex = document.getElementsByClassName("indexApi3");
+    var upcommingRocketIdIndex = document.getElementsByClassName("indexApi4");
+    var upcommingLaunchSiteIndex = document.getElementsByClassName("indexApi5");
+
+    let output = ""
+    for (var i = 0; i < api1.length; i++) {
+        output += "<div>" + api1[i].message + "</div>";
+    }
+    // Add mission name from api
+    for (var i = 0; i < upcommingHeading.length; i++) {
+        upcommingHeading[i].innerHTML = "<h3>Mission name: " + api1[i].mission_name + "</h3>";
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDetails.length; i++) {
+        upcommingDetails[i].innerHTML = api1[i].details;
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDate.length; i++) {
+        upcommingDate[i].innerHTML = "<p>Date: " + api1[i].launch_date_local + "</p>";
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingRocketId.length; i++) {
+        upcommingRocketId[i].innerHTML = "<p>Rocket Name: " + api1[i].rocket.rocket_name + "</p>";
+    }
+    for (var i = 0; i < upcommingLaunchSite.length; i++) {
+        upcommingLaunchSite[i].innerHTML = "<p>Launch site: " + api1[i].launch_site.site_name + "</p>";
+    }
+
+    // Get info for openBox in upcomming launches
+    for (var i = 0; i < upcommingHeading2.length; i++) {
+        upcommingHeading2[i].innerHTML = "<p>Mission name: " + api1[i].mission_name + "</p>";
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDetails2.length; i++) {
+        upcommingDetails2[i].innerHTML = "<p>About the mission: " + api1[i].details + "</p>";
+        if (upcommingDetails2[i] == null) {
+            upcommingDetails2[i].innerHTML = "<p></p>";
+        }
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDate2.length; i++) {
+        upcommingDate2[i].innerHTML = "<p>Launch date: " + api1[i].launch_date_local + "<p/>";
+
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingRocketId2.length; i++) {
+        upcommingRocketId2[i].innerHTML = "<p>Rocket name: " + api1[i].rocket.rocket_name + "</p>";
+    }
+    for (var i = 0; i < upcommingLaunchSite2.length; i++) {
+        upcommingLaunchSite2[i].innerHTML = "<p>Launch site : " + api1[i].launch_site.site_name_long + "</p>";
+    }
+
+    for (var i = 0; i < upcommingDate3.length; i++) {
+        upcommingDate3[i].innerHTML = "<p>Launch date: " + api1[i].launch_date_local + "<p/>";
+
+    }
+
+    for (var i = 0; i < upcommingHeadingIndex.length; i++) {
+        upcommingHeadingIndex[i].innerHTML = "<p>Mission name: " + api1[i].mission_name + "</p>";
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDetailsIndex.length; i++) {
+        upcommingDetailsIndex[i].innerHTML = "<p>About the mission: " + api1[i].details + "</p>";
+        if (upcommingDetailsIndex[i] == null) {
+            upcommingDetailsIndex[i].innerHTML = "<p></p>";
+        }
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingDateIndex.length; i++) {
+        upcommingDateIndex[i].innerHTML = "<p>Launch date: " + api1[i].launch_date_local + "<p/>";
+
+    }
+    // Add details from api
+    for (var i = 0; i < upcommingRocketIdIndex.length; i++) {
+        upcommingRocketIdIndex[i].innerHTML = "<p>Rocket name: " + api1[i].rocket.rocket_name + "</p>";
+    }
+    for (var i = 0; i < upcommingLaunchSiteIndex.length; i++) {
+        upcommingLaunchSiteIndex[i].innerHTML = "<p>Launch site : " + api1[i].launch_site.site_name_long + "</p>";
+    }
+
+    // Use upcomming date to count down next launch on index page
+
+    var countDown = new Date(api1[1].launch_date_local).getTime();
+
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var timeDifference = countDown - now;
+
+        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = "<p>Next launch into space: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s </p>";
+        if (timeDifference <= 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+
+
+    }, 1000);
+}
+request.send();
+
+// Function for displaying and hiding hamburger menu
+
+function openMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
